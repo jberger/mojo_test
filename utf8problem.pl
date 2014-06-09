f280fae2f859441fbbc7b7417867f13748f3d279
Abhishek Shende <a.shende@contractor.verio.net>
2014-06-09 18:45:11 -0400

    testing mojo for internationalization

---
+++ utf8problem.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# run with command: morbo utf8problem.pl
+
+use Mojolicious::Lite;
+use Mojo::JSON;
+use Mojo::Util qw(slurp);
+
+get '/japanese' => sub {
+    my $self     = shift;
+    my $filename = $self->app->home->rel_file('error.xml');
+    say "Reading $filename";
+
+    my $strings;
+    my $dom = Mojo::DOM->new( slurp($filename) );
+
+    foreach my $string ( $dom->find('*')->each ) {
+        next if $string->type eq 'strings';
+        $strings->{ $string->type } = $string->text;
+    }
+
+    $self->render( json => $strings );
+
+};
+
+app->start;

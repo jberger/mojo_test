09f4e3cdc9bdd8b7dcc79d557338f9f3bb24e2b3
Abhishek Shende <abhishekisnot@gmail.com>
2014-06-09 21:33:41 -0400

    check xml::libxml

--- utf8problem.pl
+++ utf8problem.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use Mojolicious::Lite;
 use Mojo::ByteStream 'b';
+use XML::LibXML;
 
 get '/japanese' => sub {
     my $self     = shift;
@@ -8,11 +9,16 @@ get '/japanese' => sub {
     say "Reading $filename";
 
     my $strings;
-    my $dom = Mojo::DOM->new( b($filename)->slurp->decode );
 
-    foreach my $string ( $dom->find('*')->each ) {
-        next if $string->type eq 'strings';
-        $strings->{ $string->type } = $string->text;
+    #my $dom = Mojo::DOM->new( b($filename)->slurp->decode );
+    my $dom = XML::LibXML->load_xml( location => $filename );
+
+    # foreach my $string ( $dom->find('*')->each ) {
+    #     next if $string->type eq 'strings';
+    #     $strings->{ $string->type } = $string->text;
+    # }
+    foreach my $string ( $dom->findnodes('/strings/*') ) {
+        $strings->{ $string->nodeName } = $string->textContent;
     }
 
     $self->render( json => $strings );
